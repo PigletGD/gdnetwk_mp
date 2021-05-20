92d32cbcdd57818e29feb77e6da73bdf82e210fe
JacobVM <43977718+PigletGD@users.noreply.github.com>
2021-05-20 20:16:21 +0800

    Adjusted C++ Server and Client to make Multi-Chat Example Program
    Modified the server to account for multiple different clients coming in and made client multi-threaded to allow for sending and receiving messages to happen concurrently.

--- GDNETWK_MP_CLIENT/GDNETWK_MP_CLIENT/main.cpp
+++ GDNETWK_MP_CLIENT/GDNETWK_MP_CLIENT/main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 #include <string>
 #include <WS2tcpip.h>
+#include <thread>
 
 #pragma comment(lib, "ws2_32.lib")
 
 std::string ipAddress = "127.0.0.1";		// IP Address of the server
 int port = 54010;							// Listening port # on the server
+
+SOCKET sock;
+
+void sendMessage() {
+	// do-while loop to send and receive data
+	std::string userInput;
+
+	do {
+		// prompt user for some text
+		//std::cout << "> ";
+		std::getline(std::cin, userInput);
+
+		if (userInput.size() > 0) { // make sure the user has typed in something
+			// send the text
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);	
+		}
+	} while (userInput.size() > 0);
+}
+
+void receiveMessage() {
+	while (true) {
+		char buf[4096];
+
+		// wait for response
+		ZeroMemory(buf, 4096);
+		int bytesReceived = recv(sock, buf, 4096, 0);
+		if (bytesReceived > 0) { // echo response to console if bytes were received
+			std::cout << "SERVER> " << std::string(buf, 0, bytesReceived) << std::endl;
+		}
+	}
+}
+
+void test() {
+	while (true) {
+		std::cout << "HAHAHAHAHAHAHAHHAHAHAHA\n";
+	}
+}
 
 int main() {
 	// initialize winsock
@@ -18,7 +56,7 @@ int main() {
 	}
 
 	// create socket
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) {
 		std::cerr << "Can't create socket, Error #" << WSAGetLastError() << ". Quitting..." << std::endl;
 		WSACleanup();
@@ -40,27 +78,10 @@ int main() {
 		return -1;
 	}
 	
-	// do-while loop to send and receive data
-	char buf[4096];
-	std::string userInput;
+	std::thread(&sendMessage).detach();
+	std::thread(&receiveMessage).detach();
 
-	do {
-		// prompt user for some text
-		std::cout << "> ";
-		std::getline(std::cin, userInput);
-
-		if (userInput.size() > 0) { // make sure the user has typed in something
-			// send the text
-			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-
-			// wait for response
-			ZeroMemory(buf, 4096);
-			int bytesReceived = recv(sock, buf, 4096, 0);
-			if (bytesReceived > 0) { // echo response to console if bytes were received
-				std::cout << "SERVER> " << std::string(buf, 0, bytesReceived) << std::endl;
-			}
-		}
-	} while (userInput.size() > 0);
+	while (true);
 
 	// gracefully close down everything
 	closesocket(sock);
--- GDNETWK_MP_SERVER/GDNETWK_MP_SERVER/main.cpp
+++ GDNETWK_MP_SERVER/GDNETWK_MP_SERVER/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "TcpListener.h"
 
@@ -8,11 +9,95 @@ int port = 54010;
 
 void Listener_MessageReceived(TcpListener* listener, int client, std::string msg);
 
-int main() {
+int oldmain() {
 	TcpListener server(ipAddress, port, Listener_MessageReceived);
 
 	if (server.init())
 		server.run();
+
+	return 0;
+}
+
+int main() {
+
+	// initialize winsock
+	WSADATA wsData; // variable to hold data when starting up winsock
+	WORD ver = MAKEWORD(2, 2); // indicates version of winsock (ver. 2.2)
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) {
+		std::cerr << "Can't initialize winsock! Quitting..." << std::endl;
+		return -1;
+	}
+
+	// create a socket
+	// AF (Address Family); SOCK_STREAM (TCP Socket)
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET) {
+		std::cerr << "Can't create a socket! Quitting..." << std::endl;
+		return 420;
+	}
+
+	// bind socket to ip address and port
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // could also use inet_pton ....
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	// tell winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(listening, &master);
+
+	while (true) {
+		fd_set copy = master;
+
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+		for (int i = 0; i < socketCount; i++) {
+			SOCKET socket = copy.fd_array[i];
+			if (socket == listening) {
+				// accept a new connection
+				SOCKET client = accept(listening, nullptr, nullptr);
+
+				// add the new connection to the list of connected clients
+				FD_SET(client, &master);
+
+				// send a welcome message to the connected client
+				std::string welcomeMsg = "Welcome to the Awesome Chat Server!\r\n";
+				send(client, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0);
+
+				// todo: broadcast we have a new connection
+			}
+			else {
+				char buf[4096];
+				ZeroMemory(buf, 4096);
+
+				int bytesIn = recv(socket, buf, 4096, 0);
+				if (bytesIn <= 0) {
+					// drop client
+					closesocket(socket);
+					FD_CLR(socket, &master);
+				}
+				else {
+					// send message to other clients, and definitely not listening socket
+					for (int i = 0; i < master.fd_count; i++) {
+						SOCKET outSock = master.fd_array[i];
+						if (outSock != listening && outSock != socket) {
+							std::ostringstream ss;
+							ss << "SOCKET #" << socket << ": " << buf << "\r\n";
+							std::string strOut = ss.str();
+							send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return 0;
 }
